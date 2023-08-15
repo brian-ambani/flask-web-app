ad25223ab0bb7d25ca4e6c97336ac4f2fa5c94fb
brian-ambani <118635495+brian-ambani@users.noreply.github.com>
2023-08-15 00:30:20 +0000

    updated the project

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Nairobi, Kenya',
+    'salary': '$ 8,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Mombasa, Kenya',
+    'salary': '$ 9,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': '$ 5,000'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'Vancouver, Canada',
+    'salary': '$ 10,000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/json")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
